288430af0be09198d23ef7cde172e477dcfc34fa
surayac <surayacsm@gmail.com>
2025-10-20 11:09:49 +0200

    fix(controller): remove unused importations

--- src/main/java/com/newspaper/newspaper/controller/UserController.java
+++ src/main/java/com/newspaper/newspaper/controller/UserController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.newspaper.newspaper.model.User;
 import com.newspaper.newspaper.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
