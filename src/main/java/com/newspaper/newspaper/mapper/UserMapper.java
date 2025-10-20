c440b88b5870ee6a72d8db704010810f1babb946
surayac <surayacsm@gmail.com>
2025-10-20 19:24:46 +0200

    chore(user): create UserMapper class without implementation

---
+++ src/main/java/com/newspaper/newspaper/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.newspaper.newspaper.mapper;
+
+import com.newspaper.newspaper.dto.UserDTO;
+import com.newspaper.newspaper.model.User;
+
+public class UserMapper {
+
+    public User toEntity(UserDTO userDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toEntity'");
+    }
+
+    public UserDTO toDTO(User savedUser) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toDTO'");
+    }
+    
+}
--- src/main/java/com/newspaper/newspaper/service/UserServiceImpl.java
+++ src/main/java/com/newspaper/newspaper/service/UserServiceImpl.java
@@ -1,24 +1,48 @@
 package com.newspaper.newspaper.service;
 
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.newspaper.newspaper.dto.UserDTO;
+import com.newspaper.newspaper.mapper.UserMapper;
 import com.newspaper.newspaper.model.User;
 import com.newspaper.newspaper.repository.UserRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
+
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserDTO createUser(UserDTO userDTO) {
+        User user = userMapper.toEntity(userDTO);
+        User savedUser = userRepository.save(user);
+        return userMapper.toDTO(savedUser);
+           }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll().stream()
+            .map(userMapper::toDTO)
+            .collect(Collectors.toList());
     }
 
     @Override
-    public ResponseEntity<User> addUser(User user) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addUser'");
+    public UserDTO getUserById(Long id) {
+      return userRepository.findById(id)
+                .map(userMapper::toDTO)
+                .orElseThrow(() -> new RuntimeException("User not found")); 
     }
 
-
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
