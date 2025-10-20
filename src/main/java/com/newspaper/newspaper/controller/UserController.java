d533bdbfd84624fc7237b007b049e226d534b941
DevErika <erikamontoya2023@gmail.com>
2025-10-20 10:31:54 +0200

    feat(controller) adding user and dependencies posgresql

--- src/main/java/com/newspaper/newspaper/controller/UserController.java
+++ src/main/java/com/newspaper/newspaper/controller/UserController.java
@@ -1,5 +1,33 @@
 package com.newspaper.newspaper.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newspaper.newspaper.model.User;
+import com.newspaper.newspaper.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+
 public class UserController {
+    
+
+    private UserService userService; 
+
+    public UserController(UserService userService) {
+        this.userService = userService; 
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
     
 }
