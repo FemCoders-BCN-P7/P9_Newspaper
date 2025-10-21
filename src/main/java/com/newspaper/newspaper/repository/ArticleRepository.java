dd095b32b0850621499d1ddbb426ef9c3934240f
Mio <gusw.0246.lpsyg@gmail.com>
2025-10-21 15:54:25 +0200

    refactor(ArticleRepository): extends JpaRepository

--- src/main/java/com/newspaper/newspaper/repository/ArticleRepository.java
+++ src/main/java/com/newspaper/newspaper/repository/ArticleRepository.java
@@ -1,6 +1,23 @@
 package com.newspaper.newspaper.repository;
 
-public interface ArticleRepository {
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.newspaper.newspaper.model.Article;
+import com.newspaper.newspaper.model.User;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    
+    
+    List<Article> findByUser(User user);
+
+    List<Article> findByCategory(String category);
+
+
 
     
     
